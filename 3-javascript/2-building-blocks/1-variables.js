aed54753ac7edb268fb50ffd3d1c0f7909790684
buivd4 <buivd4@hotmail.com>
2022-10-05 23:36:42 +0700

    add files

---
+++ 3-javascript/2-building-blocks/1-variables.js
@@ -0,0 +1,17 @@
+var soNguyen=10;
+var soThuc=3.14;
+var chuoi="DaylaChuoi";
+var mang = [1, 2, 4];
+var tuDien = {
+    "cow": "con bo",
+    "cat": "con meo"
+}
+var tuDien2 = {
+    "math": 7.0,
+    "chemistry": "not passed"
+}
+// console.log(tuDien2.chemistry)
+// console.log(tuDien2["chemistry"])
+var obj = new Array(3);
+// console.log(obj)
+
---
+++ 3-javascript/2-building-blocks/2-operator.js
@@ -0,0 +1,12 @@
+console.log(1+2);
+console.log(1-2);
+console.log(1/2);
+console.log(1*2);
+
+console.log("Nguyen " + "Van" + " A");
+
+// Boolean operator
+console.log(1==1)
+console.log(1==2)
+console.log(1>2)
+console.log(1<2)
