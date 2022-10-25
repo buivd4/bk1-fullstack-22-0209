e6834ffcadc8e98e52482d8ee33b9b4fd8123e99
buivd4 <buivd4@hotmail.com>
2022-10-25 21:27:07 +0700

    add files

---
+++ 0-practices/product-crud/app.js
@@ -0,0 +1,20 @@
+var products = [
+    {
+        id: 1,
+        name: "Mặt hàng 1",
+        description: "Mô tả 1",
+        price: 2.99
+    },
+    {
+        id: 11,
+        name: "Mặt hàng 2",
+        description: "Mô tả 2",
+        price: 2.99
+    },
+    {
+        id: 2,
+        name: "Mặt hàng 3",
+        description: "Mô tả 3",
+        price: 2.99
+    }
+]
